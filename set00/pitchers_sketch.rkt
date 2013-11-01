f8bae054d00efacb00b9bb0f296766ff39def282
Zhaoliang Liu <zhaoliangliu@gmail.com>
2013-11-01 16:33:02 -0400

    Update pitchers_sketch.rkt

--- set00/pitchers_sketch.rkt
+++ set00/pitchers_sketch.rkt
@@ -1,35 +1,59 @@
 
-successors: Listof<Configurations>-> Listof<Configurations>
-next-configurations conf1 0 0 n 
-conf2 
+
+(define (pitchers=? ps1 ps2)
+  )
 
 
-next-configuration conf i j 
+(define-struct configuration (pitchers moves))
 
-after-move, cons move
+;;member?: Picthers Listof<Pitchers> -> Boolean
+(define (member? pitchers pitchers-set)
+  (ormap 
+   (lambda (p) (pitchers=? pitchers p))
+   pitchers-set))
+
+;;successors: Listof<Configuration>-> Listof<Configuration>
+(define (successor confs)
+  (foldr append empty
+	 (map next-configurations confs)))
+
+
+(define (next-configuration config i j)
+  (local
+    (define move (make-move i j))
+    (make-configuration (pitchers-after-move (configuration-pitchers config)
+					     move)
+			(cons move (configuration-moves config)))))
+
+(define (next-configurations config i j n)
+  (if (= i (+ n 1)) empty
+      (if (= j (+ n 1))
+	  (next-configurations config (+ i 1) 1)
+	  (cons (next-configuration config i j)
+		(next-configurations config i (+ j 1) n)))))
 
 
 
-next-configurations: Configuration i j n 
-
-(define (next-configurations i j n)
-  if i==n
   
+(define (configuration-diff confs pitchers-set)
+  (filter 
+   (lambda (conf)
+     (not (member? (configuration-pitchers conf) pitchers-set)))
+   confs))
 
+;;set-diff: Listof<Configuration> Listof<Pithers> -> Listof<Configurations>
+;;
+;;Configuration is (pitchers moves)
+;;Listof<Pithers> Listof<Configurations> Graph -> MaybeListof<Move>
+  (define (solver searched newest)
+    (local
+      (define answer (filter configuration-reach-goal? newest))
+      (define searchednew (append searched (map configuration-pitchers newest)))
+      (define candidates (configuration-diff (successors newest) searchednew))
+      (if (cons? answer) (configuration-moves (first answer))
+	  (solver searchednew candidates)
+	  )))
 
-
-set-diff: Listof<Configurations> Listof<Pithers> -> Listof<Configurations>
-
-Configuration is (pitchers moves)
-Listof<Pithers> Listof<Configurations> Graph -> MaybeListof<Move>
-(define (solver searched newest)
-  (
-   (filter reach-goal? newest) not empty return moves
-    searched+=newest.getPicteher. 
-   (candidates set-diff (successors newest) searched)
-  
-   (solver searched candidates graph)
-   ))
 
 
 
