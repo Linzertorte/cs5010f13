85be7d02426c1964a0f120dac7109f0ac3c80699
Zhaoliang Liu <Linzertorte@users.noreply.github.com>
2014-01-30 23:04:27 -0500

    Update emacs.el

--- emacs.el
+++ emacs.el
@@ -1,27 +1,36 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen nil)
- '(send-mail-function (quote smtpmail-send-it))
- '(smtpmail-smtp-server "smtp.gmail.com")
- '(smtpmail-smtp-service 587))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
-
-;; add a repo to the package manager
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (require 'auto-complete)
 (global-auto-complete-mode t)
-(require 'ibus)
-(add-hook 'after-init-hook 'ibus-mode-on)
 (electric-pair-mode t)
 (show-paren-mode t)
+(require 'auto-indent-mode)
+(auto-indent-global-mode)
+(add-hook 'python-mode-hook '(lambda ()
+                               (setq python-indent 4)))
+
+
+(setq c-default-style "linux")
+
+(add-hook 'c-mode-common-hook '(lambda ()
+                                 (setq c-indent-level 4
+                                       c-basic-offset 4
+                                       indent-tabs-mode nil)))
+
+(require 'sr-speedbar)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.                                                        
+ ;; If you edit it by hand, you could mess it up, so be careful.                                     
+ ;; Your init file should contain only one such instance.                                            
+ ;; If there is more than one, they won't work right.                                                
+ '(haskell-mode-hook (quote (turn-on-haskell-indent)))
+ '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.                                                            
+ ;; If you edit it by hand, you could mess it up, so be careful.                                     
+ ;; Your init file should contain only one such instance.                                            
+ ;; If there is more than one, they won't work right.                                                
+ )
